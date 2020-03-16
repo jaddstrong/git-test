97a4254c2021fde0ecee7f2e0a0cbcda111cab54
jaddstrong <jadd@DESKTOP-JLH59PH.localdomain>
2020-03-16 14:35:48 +0800

    updated

---
+++ test/resources/views/info/info.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <ul>
+            <li>
+                <a href="/">About Me</a>
+            </li>
+            <li>
+                <a href="/info">Personal Information</a>
+            </li>
+            <li>
+                <a href="/exp">Experience</a>
+            </li>
+            <li>
+                <a href="/contact">Contact</a>
+            </li>
+        </ul>
+        <h1>Personal Information</h1>
+        <label>Full name: Castillo, Ramon Jadd Morden</label>
+        <label>Address: Tayug, Pangasinan</label>
+        <label>Age: 25</label>
+    </body>
+</html>
